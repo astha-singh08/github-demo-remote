79e52b87b05ffae52962ab758e96b3416349ec4b
astha-singh08 <99973592+astha-singh08@users.noreply.github.com>
2022-02-18 19:45:10 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+#add implementation
+def add(x,y): 
+  return x+y
+#subtract implementation
+def subtract(x,y): 
+  return x-y
+#multiply implementation
+def multiply(x,y): 
+  return x*y
+# divide implementation
+def divide(x,y): 
+  if y==0:
+      return DIVIDE_BY_0_ERROR
+  else:
+      return x/y
+#square implementation
+ def square(x):
+     pass 
